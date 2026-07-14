57ca45e0f40878ab1b78a0c07a048698fc27da1a
Sunnybathu <sunny.bathu24@sasi.ac.in>
2026-07-14 10:47:48 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const connectDB = async () => {
   try {
     // MongoDB Atlas connection string
-    const connString = "mongodb+srv://KulsumShaik24_db_user:kulsum20@cluster0.nxftcfq.mongodb.net/kulsum?appName=cluster0";
+    const connString = "mongodb+srv://josaiboppana:<db_password>@cluster0.rxrzhyp.mongodb.net/?appName=Cluster0";
     
     await mongoose.connect(connString);
     console.log('MongoDB connected successfully!');
